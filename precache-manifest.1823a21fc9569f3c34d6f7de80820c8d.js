d944e8af623a57bd1faf53e0e1cbe6133bc999f5
unknown <zoka_cs_pro@hotmail.com>
2020-09-11 19:30:01 +0200

    Updates

---
+++ precache-manifest.1823a21fc9569f3c34d6f7de80820c8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35abc8b93537d4e5380baad096ea7e1",
+    "url": "/monstersRolodex/index.html"
+  },
+  {
+    "revision": "2d15a7a8f3e2f20483cb",
+    "url": "/monstersRolodex/static/css/main.43d92dca.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monstersRolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersRolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d15a7a8f3e2f20483cb",
+    "url": "/monstersRolodex/static/js/main.2a9224d6.chunk.js"
+  },
+  {
+    "revision": "0735bc06010632169b4e",
+    "url": "/monstersRolodex/static/js/runtime-main.0dd8b790.js"
+  }
+]);
